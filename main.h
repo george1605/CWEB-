5948049c750a4ccec3d5c2e62bb155aef849ca6b
george1605 <32997165+george1605@users.noreply.github.com>
2021-04-10 11:53:30 +0300

    Create main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+#define RED_BTN "background:color:red;border:0px solid black;"
+
+#define SBUTTON "<button>"
+#define EBUTTON "</button>"
+
+class Style {
+public:
+  string value = "";
+  Style(){
+    
+  }
+  ~Style(){ value = ""; }
+}
